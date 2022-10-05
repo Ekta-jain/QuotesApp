aec32b1c4f28238e377871729c9b1561a5a0acc8
ektablackbuck <99865702+ektablackbuck@users.noreply.github.com>
2022-10-05 18:33:25 +0530

    MongoDb connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const noteRouter = require('./routes/noteRoutes');
 const userRouter = require('./routes/userRoutes');
+// for connecting mongoDb from nodeJs project we use Mongoose library
+const mongoose = require('mongoose');
 
 const app = express();
 app.use('/users', userRouter);
@@ -11,9 +13,14 @@ app.get('/', (req, res) => (
 ));
 
 
+mongoose.connect('mongodb+srv://admin:admin@cluster0.khbktxq.mongodb.net/?retryWrites=true&w=majority').then(
+    ()=>{
+      app.listen(5010, () => (
+        console.log('server strated on port 5010')
+      ));
+    },
+).catch((error) =>{
+  console.log(error);
 });
 
-app.listen(5010, () => (
-  console.log('server strated on port 5010')
-));
 
