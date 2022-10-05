09aab16372da5976897240976439dd3487c999c9
ektablackbuck <99865702+ektablackbuck@users.noreply.github.com>
2022-10-05 17:06:35 +0530

    lint added

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const req = require("express/lib/request");
-const quotes = require("./quotes.json")
+const express = require('express');
+const quotes = require('./quotes.json');
 const app = express();
 
-app.get("/",(req,res)=>(
-    res.send("sent from response")
-))
+app.get('/', (req, res) => (
+  res.send('sent from response')
+));
 
-app.get("/quote",(req,res)=>(
-    res.status(200).json(quotes)
-))
+app.get('/quote', (req, res) => (
+  res.status(200).json(quotes)
+));
 
-app.get("/random",(req,res)=>{
-    let index = Math.floor(Math.random()*quotes.length)
-    let quote = quotes[index]
-    res.status(200).json(quote)
-})
+app.get('/random', (req, res) => {
+  const index = Math.floor(Math.random() * quotes.length);
+  const quote = quotes[index];
+  res.status(200).json(quote);
+});
 
-app.listen(5010,()=>(
-    console.log("server strated on port 5010")
-))
+app.listen(5010, () => (
+  console.log('server strated on port 5010')
+));
